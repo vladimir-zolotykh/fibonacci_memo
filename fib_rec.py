810d678178875c2091b9c6615424747d1cd15c0f
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-03-16 11:02:05 +0200

    Add timethis decorator (function)

--- fib_rec.py
+++ fib_rec.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from contextlib import contextmanager
+import time
 
 
 def fib_rec(n):
@@ -9,3 +11,34 @@ def fib_rec(n):
     if n in (1, 2):
         return n
     return fib_rec(n - 1) + fib_rec(n - 2)
+
+
+@contextmanager
+def timethis(label):
+    start = time.time()
+    try:
+        yield
+    finally:
+        end = time.time()
+        print("{}: {}".format(label, end - start))
+
+
+class Timethis:
+    def __init__(self, label: str):
+        self.label = label
+
+    def __enter__(self):
+        self.start = time.time()
+
+    def __exit__(self):
+        self.end = time.time()
+        print("{}: {}".format(self.label, self.end - self.start))
+
+
+# $ python fib_rec.py
+# getting fib_rec(40): 11.172739028930664
+# getting fib_rec(42): 33.33191108703613
+
+if __name__ == "__main__":
+    with timethis("getting fib_rec(N)"):
+        fib_rec(42)
